1191c159954d6c59e936fa843a6f0f699fb3b979
ayotayo435 <poltmateo@gmail.com>
2025-12-03 22:16:42 -0500

    adsf

--- main.cpp
+++ main.cpp
@@ -240,7 +240,81 @@ int getQuestions()
 
 */
 
+void LevelFunction()
+{
+    //---------------------------------
+    /*---THIS IS THE PLAYER LOGIC---*/
+    /*----------DONT FORGET----------*/
+    //---------------------------------
 
+    while(true)
+    {  
+        
+        while (playerY == 220)
+        {
+            //don't change position
+
+            //erase old player
+            LCD.SetFontColor(BLACK);
+            //REPLACE THIS WITH THE CHARACTER ART
+            LCD.FillRectangle(playerX, playerY, 12, 12);
+
+            if (Keyboard.isPressed(KEY_W))
+            {
+                //change y position to go up
+                playerY = playerY - 5;
+            }
+    
+
+            //draw player
+            LCD.SetFontColor(WHITE);
+            //REPLACE THIS WITH THE CHARACTER ART
+            LCD.FillRectangle(playerX, playerY, 12, 12);
+            Sleep(5);
+        }
+        
+
+        while (Keyboard.isPressed(KEY_W))
+        {
+            //erase old player
+            LCD.SetFontColor(BLACK);
+            //REPLACE THIS WITH THE CHARACTER ART
+            LCD.FillRectangle(playerX, playerY, 12, 12);
+
+            //change y position to go up AND STOP AT ROOF
+            if(playerY != 10)
+            {
+            playerY = playerY - 5;
+            }
+
+            //draw player
+            LCD.SetFontColor(WHITE);
+            //REPLACE THIS WITH THE CHARACTER ART
+            LCD.FillRectangle(playerX, playerY, 12, 12);
+            Sleep(5);
+        }
+  
+        //--- GRABITY LOGIC IMPORTANT ---
+            //erase old player
+            LCD.SetFontColor(BLACK);
+            //REPLACE THIS WITH THE CHARACTER ART
+            LCD.FillRectangle(playerX, playerY, 12, 12);
+
+            //change y position to go down
+            playerY = playerY + 5;
+
+            //draw player
+            LCD.SetFontColor(WHITE);
+            //REPLACE THIS WITH THE CHARACTER ART
+            LCD.FillRectangle(playerX, playerY, 12, 12);
+            Sleep(5);
+    }
+
+    //------------------------------
+    /*--- END OF PLAYER LOGIC-----*/
+    //------------------------------
+
+}
 
 int main()
 {
